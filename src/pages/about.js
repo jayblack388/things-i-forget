6160a7eda56a2ce040a97d9ce3d0f32f03a67a52
John Blackwell <jblackwell072588@gmail.com>
2019-10-21 13:05:22 -0700

    util pages

---
+++ src/pages/about.js
@@ -0,0 +1,8 @@
+import React from "react"
+import Wrapper from "../components/layout"
+export default () => (
+  <Wrapper>
+    <h1>About me</h1>
+    <p>I’m good enough, I’m smart enough, and gosh darn it, people like me!</p>
+  </Wrapper>
+)
